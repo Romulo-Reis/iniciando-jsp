6052581875cf74ce49d03bcade5b462f17fab9bf
Romulo-Reis <romuloreistavares@hotmail.com>
2017-08-02 23:55:25 -0300

    Commit do projeto do curso iniciando com jsp do site school of net.

---
+++ SON-JSP/src/MyFilter.java
@@ -0,0 +1,48 @@
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+
+import sun.util.locale.provider.TimeZoneNameUtility;
+
+@WebFilter("/*")
+public class MyFilter implements Filter{
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		Long initTime = System.currentTimeMillis();
+		
+		String endPoint = ((HttpServletRequest)request).getRequestURI();
+		
+		chain.doFilter(request, response);
+		
+		Long endTime = System.currentTimeMillis();
+		
+		Long result = endTime - initTime;
+		
+		System.out.println(String.format("%d", TimeUnit.MILLISECONDS.toSeconds(result)));
+		
+	}
+	
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
